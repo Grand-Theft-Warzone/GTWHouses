579dc47293a6b1dc45c390015e85a6f3ff032266
Edgar Silva <edgarasdaa10@gmail.com>
2023-09-27 12:45:14 +0100

    Project Setup

---
+++ src/main/java/com/edgarssilva/gwthouses/GWTHouses.java
@@ -0,0 +1,17 @@
+package com.edgarssilva.gwthouses;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GWTHouses extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
