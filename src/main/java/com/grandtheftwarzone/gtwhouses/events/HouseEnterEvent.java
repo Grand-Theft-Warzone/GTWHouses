2705fe6aa13e6a0365fd85b88cea57bae1b15aa0
edgarssilva <edgarasdaa10@gmail.com>
2023-10-09 20:28:52 +0100

    House enter improvement.

--- src/main/java/com/grandtheftwarzone/gtwhouses/events/HouseEnterEvent.java
+++ src/main/java/com/grandtheftwarzone/gtwhouses/events/HouseEnterEvent.java
@@ -11,7 +11,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.scheduler.BukkitTask;
 
 import java.util.HashMap;
 import java.util.UUID;
